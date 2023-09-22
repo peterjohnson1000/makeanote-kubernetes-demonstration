3f6d62feac731c9f1f277f84a399c38d86ed6cd4
peterjohnson1000 <peterj1298@gmail.com>
2023-09-22 13:22:53 +0530

    commit message

---
+++ Client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComponent from './components/App';
+import './styles/app.scss';
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<AppComponent />, document.getElementById('app'));
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm run serve:api &
+
+echo "npm run serve:api has started successfully"
+
+npm run serve:dev
